db75ee06b6528ca085bd9cd67ce7bc9ad737df0a
kamiryu-naki07 <68836396+kamiryu-naki07@users.noreply.github.com>
2020-08-03 00:57:14 +0900

    finsh
    a

---
+++ common.js
@@ -0,0 +1,15 @@
+/*$(window).on('load resize', function(){
+	if($(window).width() > 600){
+		if($('body'){				
+			// ワークスのオンマウス
+			$('.Works a').hover(function(){
+				$(this).parent().children('h2, p').stop().fadeTo(500, 1).shuffleLetters();
+				},
+				
+			function(){
+				$(this).parent().children('h2, p').stop().fadeTo(300, 0);
+				});
+			}
+		}
+	});
+*/
